6621162f127dbebd4b7d4a15e12590a1f9109fdd
erfani24 <113503524+erfani24@users.noreply.github.com>
2023-02-16 21:31:54 +0330

    blog app added
    template and urls  configured

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from .views import *
+
+app_name = "blog"
+
+urlpatterns = [
+    path('',blog_view, name="index"),
+    path('single/',blog_single,name='blog-single'),
+    
+    
+]
