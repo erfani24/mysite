4f2af2a6b5b1c37b26713a2262c53b11e691b36f
Mohammad Erfani <113503524+erfani24@users.noreply.github.com>
2023-02-20 19:45:48 +0330

    create models
    post model
    contact model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # image
+    # category
+    # tag
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now=True)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    email = models.EmailField()
+    message =models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
