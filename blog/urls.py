812fe22845b159a04bee94d533f3a20b111f5635
Mohammad Erfani <113503524+erfani24@users.noreply.github.com>
2023-02-22 20:42:30 +0330

    dynamic url
    a test  page with dynamic url added.

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ app_name = "blog"
 urlpatterns = [
     path('',blog_view, name="index"),
     path('single/',blog_single,name='blog-single'),
+    path('post-<int:pid>/',test)
     
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html',context)
 
 def blog_single (request):
     return render(request, 'blog/blog-single.html')
+
+def test(request,pid):
+    post = Post.objects.get(id=pid)
+    context = {'post': post}
+    return render(request,'blog/test.html', context)
